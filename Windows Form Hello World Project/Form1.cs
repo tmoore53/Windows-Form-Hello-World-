a9fe29cb68c6922f7e740e88216ca457196336dd
Tyler Moore <tyler.moore53@outlook.com>
2020-03-13 14:21:26 -0700

    Commit to repo

---
+++ Windows Form Hello World Project/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form_Hello_World_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //Close the form
+            this.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //Show the Open File Dialog. If the user clicks ok, load the
+            //picture that the user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Clear Picture.
+            pictureBox1.Image = null;
+
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            //Show the color dialog box. If the user clicks OK, change the
+            //pictureBox control's  background to the color the user choses.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //If the user selects the stretch check box,
+            //change the PictureBox's
+            //Size mode in property to 'stretch.' If the user clears 
+            //the check box, change it to normal
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
